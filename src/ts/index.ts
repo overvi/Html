094e334191e7d3f2689f6607d47b6663e26b8941
Ali <aliwebdevf@gmail.com>
2024-12-08 11:59:16 +0330

    init

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -44,4 +44,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // Animations
+
+  const showcase = document.querySelector(".home-showcase");
+  const fadeText = document.querySelector(".fadeText") as HTMLDivElement;
+
+  showcase?.addEventListener("mouseover", () => {
+    fadeText?.animate(
+      [
+        {
+          opacity: "1",
+        },
+        {
+          opacity: "0",
+        },
+        {
+          opacity: "1",
+        },
+      ],
+      {
+        duration: 900,
+        iterations: 1,
+      }
+    );
+  });
+
+  showcase?.addEventListener("mouseleave", () => {
+    fadeText?.classList.remove("blink");
+    fadeText?.animate(
+      [
+        {
+          opacity: fadeText.style.opacity,
+        },
+        {
+          opacity: "1",
+        },
+      ],
+      {
+        duration: 900,
+        iterations: 1,
+      }
+    );
+  });
 });
