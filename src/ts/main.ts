43e1dc4f7a37788987a563cc7aa76e11a652d494
Ali <aliwebdevf@gmail.com>
2025-01-08 20:53:42 +0330

    h

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,15 +1,42 @@
 import "@/css/output.css";
 
+const transitionManager = () => {
+  // Create HTML style element with CSS selector that targets all
+  // elements and applies CSS to disable transitions
+  const style = document.createElement("style");
+  const css = document.createTextNode(`* {
+     -webkit-transition: none !important;
+     -moz-transition: none !important;
+     -o-transition: none !important;
+     -ms-transition: none !important;
+     transition: none !important;
+  }`);
+  style.appendChild(css);
+
+  // Create functions for adding and remove the style element from
+  // the page <head> tag
+  const enable = () => document.head.removeChild(style);
+  const disable = () => document.head.appendChild(style);
+  return { enable, disable, style };
+};
+
 function toggleTheme(
   activeToggle: HTMLElement,
   inactiveToggle: HTMLElement,
   theme: string
 ) {
+  const transitions = transitionManager();
+  transitions.disable();
+
   inactiveToggle?.classList.remove("bg-primary-400");
   activeToggle.classList.add("bg-primary-400");
 
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("colorMode", theme);
+
+  window.getComputedStyle(transitions.style).opacity;
+
+  transitions.enable();
 }
 
 // Change Theme
